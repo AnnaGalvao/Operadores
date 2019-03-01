ad8068ff7f94bde788ffde8fdabd94e29e3463e8
Anna Galvão <annaegp@hotmail.com>
2019-02-28 22:08:56 -0300

    acrescentando operadores

--- Operators/Operators/Program.cs
+++ Operators/Operators/Program.cs
@@ -10,6 +10,67 @@ namespace Operators
     {
         static void Main(string[] args)
         {
+            int a = 1;
+            int b = 2;
+            // aritméticos
+
+            //add
+            int c = a + b;
+            //Console.WriteLine
+
+            //sub
+            int subt = a - b;
+
+            //mult
+            int mult = a * b;
+
+            //divis
+            int divis = a / b;
+
+            //rest (resto da divisao) 
+            int rest = 5 % 2;
+
+            //primary
+            //incremento x++
+            //decremento x--
+
+            //oeradores comparativos
+
+            if (a > b)
+                Console.WriteLine("a > b");
+
+            if (b < a)
+                Console.WriteLine("b < a");
+
+            //=> operador lambida
+            if (a >= b) // maior igual
+                Console.WriteLine("a >= b");
+
+            if (b <= a) // menor igual
+                Console.WriteLine("b <= a");
+
+            //equals
+            if (a == b)
+                Console.WriteLine("a == b");
+
+            //not
+            if (a != b)
+                Console.WriteLine("a != b");
+
+            //logicos
+            //XOR - logicos exclusivo - um ou outro
+            if (true ^ false)
+                Console.WriteLine("true");
+
+            //condicionais
+
+            //AND aglutinar e verificar se duas condições são verdadeiras
+            if (true && true)
+                Console.WriteLine("True");
+
+            //OR (ou uma ou outra)
+            if (false || true)
+                Console.WriteLine("true");
         }
     }
 }

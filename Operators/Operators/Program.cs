47d950bda4b85c21c9096f8e298f01cd849dd2fa
Anna Galvão <annaegp@hotmail.com>
2019-02-28 22:42:22 -0300

    inclusão de operações matemáticas e uso do #region

--- Operators/Operators/Program.cs
+++ Operators/Operators/Program.cs
@@ -10,6 +10,9 @@ namespace Operators
     {
         static void Main(string[] args)
         {
+
+            #region Sintaxe
+
             int a = 1;
             int b = 2;
             // aritméticos
@@ -37,40 +40,70 @@ namespace Operators
             //oeradores comparativos
 
             if (a > b)
-                Console.WriteLine("a > b");
+                //Console.WriteLine("a > b");
 
-            if (b < a)
-                Console.WriteLine("b < a");
+                if (b < a)
+                    //Console.WriteLine("b < a");
 
-            //=> operador lambida
-            if (a >= b) // maior igual
-                Console.WriteLine("a >= b");
+                    //=> operador lambida
+                    if (a >= b) // maior igual
+                                //Console.WriteLine("a >= b");
 
-            if (b <= a) // menor igual
-                Console.WriteLine("b <= a");
+                        if (b <= a) // menor igual
+                                    //Console.WriteLine("b <= a");
 
-            //equals
-            if (a == b)
-                Console.WriteLine("a == b");
+                            //equals
+                            if (a == b)
+                                //Console.WriteLine("a == b");
 
-            //not
-            if (a != b)
-                Console.WriteLine("a != b");
+                                //not
+                                if (a != b)
+                                    //Console.WriteLine("a != b");
 
-            //logicos
-            //XOR - logicos exclusivo - um ou outro
-            if (true ^ false)
-                Console.WriteLine("true");
+                                    //logicos
+                                    //XOR - logicos exclusivo - um ou outro
+                                    if (true ^ false)
+                                        //Console.WriteLine("true");
 
-            //condicionais
+                                        //condicionais
 
-            //AND aglutinar e verificar se duas condições são verdadeiras
-            if (true && true)
-                Console.WriteLine("True");
+                                        //AND aglutinar e verificar se duas condições são verdadeiras
+                                        if (true && true)
+                                            //Console.WriteLine("True");
 
-            //OR (ou uma ou outra)
-            if (false || true)
-                Console.WriteLine("true");
+                                            //OR (ou uma ou outra)
+                                            if (false || true)
+                                                //Console.WriteLine("true");
+
+                                                #endregion
+
+            Console.WriteLine("DIGITE OS VALORES");//erro nessa linha
+
+            Console.WriteLine("Valor 1: ");
+            int v1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Valor 2: ");
+            int v2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Soma: " + Convert.ToString(v1 + v2));
+            Console.WriteLine("Subtração: " + Convert.ToString(v1 - v2));
+            Console.WriteLine("Multiplicação: " + Convert.ToString(v1 * v2));
+            Console.WriteLine("Divisão: " + Convert.ToString(v1 / v2));
+            Console.WriteLine();
+
+
+            if (v1 > 0 && v2 > 0)
+                Console.WriteLine("Valores v1 e v2 maiores que zero");
+
+            if (v1 + v2 <= 0 || v1 - v2 <= 0 || v1 / v2 <= 0)
+                Console.WriteLine("Uma ou mais operações possui valor menor ou igual a zero");
+
+
+            Console.ReadKey();
+
+           // System.Math.(...) = biblioteca de dados matematicos. diversas operações
+
+
         }
     }
 }
